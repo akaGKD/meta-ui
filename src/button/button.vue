c272fc9a89630c6c254abb02a5446dad0c0f6c2e
justin <paperwork@zsct.com>
2022-04-10 18:27:24 +0800

    testone

---
+++ src/button/button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button :class="[typeClass,sizeClass]"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    typeClass() {
+      return {
+        "meta-button-default": this.type == "" || this.type == "default",
+      };
+    },
+    sizeClass(){
+        return {
+            "meta-button-size-large": this.type == ""
+        }
+    }
+  },
+};
+</script>
+
+<style>
+</style>
